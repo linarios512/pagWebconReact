c50836ec11c0c9a0687ffbadf41e758a2e9a7ec1
linarios512 <linarios512>
2023-10-22 21:59:56 -0500

    CreandoRepositorio

---
+++ src/Home/Home.jsx
@@ -0,0 +1,109 @@
+import './Home.css'
+export function Home(){
+
+    return(
+        <>
+        <header>
+        <nav className="navbar navbar-expand-lg navbar-dark menu fixed-top">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">
+      Navbar
+    </a>
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">
+            Home
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Link
+          </a>
+        </li>
+        <li className="nav-item dropdown">
+          <a
+            className="nav-link dropdown-toggle"
+            href="#"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Dropdown
+          </a>
+          <ul className="dropdown-menu">
+            <li>
+              <a className="dropdown-item" href="#">
+                Action
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">
+                Another action
+              </a>
+            </li>
+            <li>
+              <hr className="dropdown-divider" />
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">
+                Something else here
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" aria-disabled="true">
+            Disabled
+          </a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input
+          className="form-control me-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        />
+        <button className="btn btn-outline-success" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+        </header>
+        <div className="banner text-white fw-bold">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis animi soluta repellat quasi suscipit. Dicta ipsum quae, aliquid beatae laboriosam sint cumque quasi, itaque inventore sapiente molestiae quibusdam! Commodi, recusandae!</p>
+        
+        <button className='btn btn-outline-warning'>Conocenos</button>
+        </div>
+         <div className="container-fluid fondo text-black">
+          <div className="row p-5">
+            <div className="col-12 align-self-center text-center">
+              <h5>LA CASITA DEL HORROR</h5>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis natus quod pariatur nam quae nesciunt sapiente nihil temporibus. Molestiae quod veritatis ipsa dolorem adipisci facilis itaque cupiditate sapiente maxime optio?</p>
+              <img src="../../src/assets/img/fotologo.png" alt="foto" className='img-fluid' />
+            </div>
+            
+          </div>
+          <div className="row"></div>
+         </div>
+         <div className="banner2"></div>
+
+        </>
+    )
+}
+
